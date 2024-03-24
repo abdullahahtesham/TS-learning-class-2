50c07cf68262ec2c876a549a09f086893640919c
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-03-24 15:39:23 +0500

    update it

---
+++ main.js
@@ -0,0 +1,29 @@
+var name1 = "hello world";
+//no use of space in vairable
+//in double quotation is the string
+console.log(name1);
+//tsc main.ts & node main.js (to run both in the one step)
+//"=" assigning opertor it only assign 
+name1 = "working"; //to edit or change the valus of the varible we have to write like this  
+console.log(name1);
+//compiler will read the lines by line by line
+//three way to write the varible
+//camelCase
+//snake_case
+//PascalCase
+//statement is the line of the every code and have the different 
+//Semi colon is the termintor in ts we will write or will not write the semicolon 
+//To write the two statements we will use the semicolon
+// to tell the type of the varible we use the colon 
+// if we tell this once then 
+// when we use double or single kot then it will be the string 
+var fullName = "Abdullah Ahtesham";
+// fullName =10
+//  we have to always tell the type of the varibe
+var name2 = "abdulah";
+var age = 17;
+var isMarried = true;
+var abc = undefined; // this means that the value can be anything but we do not know what it is going to be 
+var xyz = null; // null means that there is no value here
+// to make the constant varible we use the const and this will not change in next 
+var pi = 3.142;
---
+++ main.ts
@@ -0,0 +1,31 @@
+let name1 = "hello world"
+//no use of space in vairable
+//in double quotation is the string
+console.log(name1)
+//tsc main.ts & node main.js (to run both in the one step)
+//"=" assigning opertor it only assign 
+
+name1 = "working"  //to edit or change the valus of the varible we have to write like this  
+console.log(name1)
+//compiler will read the lines by line by line
+//three way to write the varible
+//camelCase
+//snake_case
+//PascalCase
+//statement is the line of the every code and have the different 
+//Semi colon is the termintor in ts we will write or will not write the semicolon 
+//To write the two statements we will use the semicolon
+// to tell the type of the varible we use the colon 
+// if we tell this once then 
+// when we use double or single kot then it will be the string 
+let fullName ="Abdullah Ahtesham"
+// fullName =10
+//  we have to always tell the type of the varibe
+let name2 :string="abdulah"
+let age :number=17
+let isMarried:boolean=true 
+let abc:undefined= undefined  // this means that the value can be anything but we do not know what it is going to be 
+let xyz:null =null   // null means that there is no value here
+// to make the constant varible we use the const and this will not change in next 
+const pi =3.142
+
